66e0d077e5f30e6eb3c2e878999d3694cddf1b85
rjgonzalez <rjgonzalez@ITEM-S59989.emea.msad.sopra>
2019-09-10 12:18:35 +0200

    First commit

---
+++ src/main/java/com/rjgonzalez/moviesonboarding/MoviesOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.rjgonzalez.moviesonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesOnboardingApplication.class, args);
+	}
+
+}
