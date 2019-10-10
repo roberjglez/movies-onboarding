92830ce3d7139530b210feb22ff0315aedf0ce66
rjgonzalez <rjgonzalez@ITEM-S59989.emea.msad.sopra>
2019-10-10 09:11:41 +0200

    Eureka dependency and annotation

--- src/main/java/com/rjgonzalez/moviesonboarding/MoviesOnboardingApplication.java
+++ src/main/java/com/rjgonzalez/moviesonboarding/MoviesOnboardingApplication.java
@@ -3,8 +3,10 @@ package com.rjgonzalez.moviesonboarding;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MoviesOnboardingApplication {
 
